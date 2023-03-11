4db87d42b86396557ad02b4d095569147b8fbb18
Sreelekshmiust <Sreelekshmi.Kumar@ust.com>
2023-03-11 12:14:13 +0530

    first commit of module1 java code

--- chumma/src/chumma/Chumma.java
+++ chumma/src/chumma/Chumma.java
@@ -4,13 +4,17 @@ public class Chumma {
 
 	public static void main(String[] args) {
 				
-				String [][] argCopy = new String[2][2];
+				String [][] argCopy = new String[1][1];
 				int x;
-				argCopy[0] = args;
+				System.out.println(argCopy[0].length);
+				argCopy[0]= args;
+				//argCopy[1]= args;
+				System.out.println(argCopy[0].length);
+				//System.out.println(argCopy[1].length);
 				x=argCopy[0].length;
-				for(int y=0;y<x;y++) {
-					System.out.println(" " + argCopy[0][y]);
-				}
+				/*
+				 * for(int y=0;y<x;y++) { System.out.println(" " + argCopy[0][y]); }
+				 */
 
 			}
 
